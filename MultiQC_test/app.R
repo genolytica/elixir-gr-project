d653e2b4d1284959f4054d87f72f79263b1322e7
pmoulos <pmoulos@gmail.com>
2019-09-14 10:41:55 +0300

    iframe solution, some times works some times not

--- MultiQC_test/app.R
+++ MultiQC_test/app.R
@@ -1,22 +1,39 @@
 library(shiny)
-library(shinyBS)
+#library(shinyBS)
+
 ui <- fluidPage(
-    actionButton(
-        inputId="showFastqc",
-        label="Dataset FastQC",
-        class="btn-sample-select"
-    ),
-    tags$head(tags$style(".modal-dialog{ width:1400px}")),
-    bsModal(id="fqc", title="FastQC", trigger="click",
-        includeHTML("www/GSE59017_multiqc_report.html")
-    )  
+	tags$head(tags$style(".modal-dialog{ width:1400px}")),
+	navbarPage(
+        id="test",
+        title="MultiQC test",
+        tabPanel("Button",
+            actionButton(
+				inputId="showFastqc",
+				label="Dataset FastQC",
+				class="btn-sample-select"
+			)
+        ),
+        tabPanel("Static"#,
+            #includeHTML("www/GSE59017_multiqc_report.html")
+        )
+    )
 )
 
-
 server <- function(input, output, session) {
-    observeEvent(input$showFastqc, {
-        toggleModal(session, "fqc", toggle="toggle")
-    })
+	observeEvent(input$showFastqc, {
+		showModal(modalDialog(
+			title="Test",
+			#includeHTML("www/GSE59017_multiqc_report.html")
+			tags$iframe(
+				src="GSE59017_multiqc_report.html",
+				width="100%",height=640,
+				#sandbox=paste("allow-forms allow-popups allow-pointer-lock",
+				#	"allow-same-origin allow-scripts allow-top-navigation"),
+				frameborder=0
+			),
+			size="l"
+		))
+	})
 }
 
 shinyApp(ui, server)
